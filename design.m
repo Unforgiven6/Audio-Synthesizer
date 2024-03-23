e989b0eacef26e18e3b1cbd52402b464d19c500e
Nick Ji <80782918+Unforgiven6@users.noreply.github.com>
2024-03-22 22:17:50 -0700

    Update design.m

--- design.m
+++ design.m
@@ -31,20 +31,22 @@ xlabel('Time (s)');
 ylabel('Amplitude');
 title('Filtered Signal');
 
-% Create a slider for adjusting the cutoff frequency
+% Step 6: Create a slider for adjusting the cutoff frequency
 fig = figure;
+max_cutoff_freq = sampling_freq / 2; % Nyquist frequency
+min_cutoff_freq = 0.1; % Set a small positive value for the minimum cutoff frequency
 slider = uicontrol('Style', 'slider', ...
-                   'Min', slider_range(1), 'Max', slider_range(2), ...
+                   'Min', min_cutoff_freq, 'Max', max_cutoff_freq, ...
                    'Value', initial_slider_value, ...
                    'SliderStep', slider_step, ...
                    'Position', [20 20 300 20], ...
-                   'Callback', @(source, event) slider_callback(source, event, t, signal));
+                   'Callback', @(source, event) slider_callback(source, event, t, signal, max_cutoff_freq));
 
 % Store filter_order, sampling_freq, signal, and t as data associated with the figure handle
 guidata(fig, struct('filter_order', filter_order, 'sampling_freq', sampling_freq, 'signal', signal, 't', t));
 
-% Define callback function for slider
-function slider_callback(source, event, t, signal)
+% Step 7: Define callback function for slider
+function slider_callback(source, ~, t, signal, max_cutoff_freq)
     % Retrieve filter_order, sampling_freq, signal, and t from figure handle data
     data = guidata(source);
     filter_order = data.filter_order;
@@ -52,12 +54,20 @@ function slider_callback(source, event, t, signal)
     signal = data.signal;
     t = data.t;
     
-    new_cutoff_freq = get(source, 'Value');
+    % Normalize cutoff frequency
+    normalized_cutoff_freq = get(source, 'Value') / max_cutoff_freq; 
+    
+    % Denormalize cutoff frequency to actual frequency
+    new_cutoff_freq = normalized_cutoff_freq * sampling_freq / 2;
+    
+    % Design filter using denormalized cutoff frequency
     updated_filter = designfilt('lowpassfir', ...
                                 'FilterOrder', filter_order, ...
                                 'CutoffFrequency', new_cutoff_freq, ...
                                 'SampleRate', sampling_freq);
-    filtered_signal = updated_filter.filter(signal);
+    
+    % Apply the filter to the signal
+    filtered_signal = filter(updated_filter, signal);
     
     % Plot re-filtered signal
     subplot(2,1,2);
